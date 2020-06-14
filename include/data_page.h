dbe405921dccb6fa466edda9e6970eb1cc451e59
Steven He <hez2010@outlook.com>
2020-06-14 09:00:54 +0000

    experiment

--- include/data_page.h
+++ include/data_page.h
@@ -1,6 +1,7 @@
 #ifndef DATA_PAGE
 #define DATA_PAGE
-
+#include <libpmem.h>
+#include <vector>
 #define DATA_PAGE_SLOT_NUM 16
 // use pm_address to locate the data in the page
 
@@ -8,7 +9,10 @@
 // one slot stores one bucket of PmEHash
 typedef struct data_page {
     // fixed-size record design
+    std::vector<std::vector<unsigned int>> pages[1000];
+
     // uncompressed page format
-} data_page;
+
+    } data_page;
 
 #endif
--- src/pm_ehash.cpp
+++ src/pm_ehash.cpp
@@ -1,4 +1,5 @@
 #include"pm_ehash.h"
+#include<libpmem.h>
 
 /**
  * @description: construct a new instance of PmEHash in a default directory
@@ -59,8 +60,6 @@ int PmEHash::update(kv kv_pair) {
 int PmEHash::search(uint64_t key, uint64_t& return_val) {
     return 0;
 }
-<<<<<<< HEAD
-jjjsdcsdcjjasdasd
 
 /**
  * @description: 用于对输入的键产生哈希值，然后取模求桶号(自己挑选合适的哈希函数处理)
