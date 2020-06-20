5e7c3f557426217a60a7e2400014c2fbd07af2ba
wenjy23 <65848855+wenjy23@users.noreply.github.com>
2020-06-20 16:59:50 +0800

    最新最新的代码

--- include/data_page.h
+++ include/data_page.h
@@ -2,15 +2,16 @@
 #define DATA_PAGE
 
 #define DATA_PAGE_SLOT_NUM 16
-#define BUCKET_SLOT_NUM 15
-#define DEFAULT_CATALOG_SIZE 16
-#define META_NAME "pm_ehash_metadata";
-#define CATALOG_NAME "pm_ehash_catalog";
-#define PM_EHASH_DIRECTORY ""; // add your own directory path to store the pm_ehash
-#define MAX_PAGE_NUM 1000      //定义最大的页面数为1000
+#define BUCKET_SLOT_NUM               15
+#define DEFAULT_CATALOG_SIZE      16
+#define META_NAME                                "pm_ehash_metadata";
+#define CATALOG_NAME                        "pm_ehash_catalog";
+#define PM_EHASH_DIRECTORY        "";        // add your own directory path to store the pm_ehash
+#define MAX_PAGE_NUM             1000        //��������ҳ����Ϊ1000
+#define PERSIST_PATH             "/mnt/pmemdir/data/"
 
-using std::map;
 using std::queue;
+using std::map;
 
 /*
 ---the physical address of data in NVM---
@@ -35,48 +36,56 @@ typedef struct kv
 typedef struct pm_bucket
 {
     uint64_t local_depth;
-    uint8_t bitmap[BUCKET_SLOT_NUM];
+    uint8_t  bitmap[BUCKET_SLOT_NUM];
     //uint8_t  bitmap[BUCKET_SLOT_NUM / 8 + 1];      // one bit for each slot
-    kv slot[BUCKET_SLOT_NUM]; // one slot for one kv-pair
-    struct pm_bucket *next;
+    kv       slot[BUCKET_SLOT_NUM];                                // one slot for one kv-pair
+    struct pm_bucket* next;
 
-    pm_bucket() : next(nullptr)
-    {
+    struct pm_bucket() : next(NULL) {
         memset(bitmap, 0, sizeof(bitmap));
     }
 } pm_bucket;
 
 typedef struct ehash_catalog
 {
-    pm_address *buckets_pm_address;     // pm address array of buckets
-    pm_bucket *buckets_virtual_address; // virtual address array mapped by pmem_map
+    pm_address* buckets_pm_address;         // pm address array of buckets
+    pm_bucket* buckets_virtual_address;    // virtual address array mapped by pmem_map
 } ehash_catalog;
 
 typedef struct ehash_metadata
 {
-    uint64_t max_file_id;  // next file id that can be allocated
-    uint64_t catalog_size; // the catalog size of catalog file(amount of data entry)
-    uint64_t global_depth; // global depth of PmEHash
+    uint64_t max_file_id;      // next file id that can be allocated
+    uint64_t catalog_size;     // the catalog size of catalog file(amount of data entry)
+    uint64_t global_depth;   // global depth of PmEHash
 } ehash_metadata;
 // use pm_address to locate the data in the page
 
 // uncompressed page format design to store the buckets of PmEHash
 // one slot stores one bucket of PmEHash
-typedef struct data_page
-{
+typedef struct data_page {
     // fixed-size record design
     // uncompressed page format
-    //一个数据页面要定义页面号， 记录哪些槽可以用，哪些槽不能用的位图，以及存放数据的槽
-    //和那个示意图是一样的
+    //һ������ҳ��Ҫ����ҳ��ţ� ��¼��Щ�ۿ����ã���Щ�۲����õ�λͼ���Լ�������ݵĲ�
+    //���Ǹ�ʾ��ͼ��һ����
     pm_bucket buckets[DATA_PAGE_SLOT_NUM];
     bool bit_map[DATA_PAGE_SLOT_NUM];
     int page_id;
 
-    data_page()
-    {
+    struct datapage() {
         memset(bit_map, 0, sizeof(bit_map));
     }
 } data_page;
 
-std::vector<data_page *> page_record;
+bool is_full(const bool bit_map[], int size);           //�ж�����һ��bit_map���Ƿ��п��õ�λ��
+
+data_page* create_new_page(int id);
+
+void init_page_from_file();
+
+std::vector<data_page*>  page_record;
+
+std::vector<pm_address*> pm_address_record;
+
+std::vector<ehash_catalog*> ehash_catalog_record;
+
 #endif
